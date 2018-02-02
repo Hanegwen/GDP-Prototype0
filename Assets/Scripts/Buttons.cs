4d77731e3d6075e8ec8400679f11b60939490f49
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-02-02 17:15:57 -0600

    Add files via upload
    Added script for Scene Changing

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
